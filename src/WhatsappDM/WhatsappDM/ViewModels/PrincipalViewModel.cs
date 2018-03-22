bb9b88a45a80452484d1218caa2757c2d1341d93
Cristopher Jr. Vivieca Ramos <cristopherviviecara025@gmail.com>
2018-03-22 16:09:56 -0400

    First Commit

---
+++ src/WhatsappDM/WhatsappDM/ViewModels/PrincipalViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WhatsappDM.ViewModels
+{
+    public class PrincipalViewModel : BaseViewModel
+    {
+        public PrincipalViewModel()
+        {
+            Title = "WhatsappDM";
+        }
+
+    }
+}
