48683cb507bb52521182e9fde43183c5816a87f1
Cristopher Jr. Vivieca Ramos <cristopherviviecara025@outlook.com>
2018-03-24 23:18:54 -0400

    Improving PrincipalPage Desing

--- src/WhatsappDM/WhatsappDM/ViewModels/AboutViewModel.cs
+++ src/WhatsappDM/WhatsappDM/ViewModels/AboutViewModel.cs
@@ -1,19 +1,18 @@
 ﻿using System;
 using System.Windows.Input;
-
 using Xamarin.Forms;
 
 namespace WhatsappDM.ViewModels
 {
     public class AboutViewModel : ContentPage
     {
+        public ICommand OpenWebCommand { get; }
+
         public AboutViewModel()
         {
             Title = "About";
 
-            OpenWebCommand = new Command(() => Device.OpenUri(new Uri("https://viviecar.com")));
+            OpenWebCommand = new Command(() => Device.OpenUri(new Uri("https://blog.viviecar.com/es/")));
         }
-
-        public ICommand OpenWebCommand { get; }
     }
 }
--- src/WhatsappDM/WhatsappDM/ViewModels/PrincipalViewModel.cs
+++ src/WhatsappDM/WhatsappDM/ViewModels/PrincipalViewModel.cs
@@ -1,13 +1,18 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace WhatsappDM.ViewModels
 {
     public class PrincipalViewModel : ContentPage
     {
+        public ICommand OpenWebCommand { get; }
+
         public PrincipalViewModel()
         {
             Title = "WhatsappDM";
-        }
 
+            OpenWebCommand = new Command(() => Device.OpenUri(new Uri("whatsapp://send?phone=18099845274")));
+        }
     }
 }
